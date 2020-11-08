fefbf7931a49b33749ea08eff07c220c521d5ccf
orshalit <orshalit93@gmail.com>
2020-11-08 15:42:14 +0200

    checking

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React, { useRef, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 //import './App.css';
-
+/**/
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
@@ -41,12 +41,13 @@ const firebaseConfig = {
   : firebase.app().firestore()*/
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+
+if (!firebase.apps.length){
+  firebase.initializeApp(firebaseConfig);  
+}
 
 const auth = firebase.auth();
-const firestore = firebase.firestore();
-
-
+const firestore = firebase.firestore()
 
 function App() {
   
@@ -66,5 +67,26 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+function SignIn() {
+
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+
+  return (
+    <>
+      <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
+      <p>Do not violate the community guidelines</p>
+    </>
+  )
+
+}
+
+function SignOut() {
+  return auth.currentUser && (
+    <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
+  )
+}
 
 export default App;
